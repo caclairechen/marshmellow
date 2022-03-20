495a9a8db10f71afd0eae226c741224154d88699
Claire Chen <claire.chen@ubisoft.com>
2022-03-20 14:45:17 -0500

    load pets list

--- frontend/src/pages/pets.vue
+++ frontend/src/pages/pets.vue
@@ -3,6 +3,9 @@
     <div data-app id="app">
       <Navigation />
       <span>My Pets</span>
+      <div v-for="pet in pets" v-bind:key="pet.id">
+        <h2>{{ pet.name }}</h2>
+      </div>
       <AddPet />
     </div>
   </v-app>
@@ -16,6 +19,14 @@ export default {
   components: {
     Navigation,
     AddPet,
+  },
+  computed: {
+    pets() {
+      return this.$store.state.pets;
+    },
+  },
+  created() {
+    this.$store.dispatch("loadPets");
   },
 };
 </script>
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -14,6 +14,9 @@ export default new Vuex.Store({
     SET_USERS(state, val) {
       state.users = val;
     },
+    SET_PETS(state, val) {
+      state.pets = val;
+    },
     ADD_PET(state, val) {
       state.pets.add(val);
     },
@@ -21,16 +24,25 @@ export default new Vuex.Store({
   actions: {
     async loadUsers({ commit }) {
       try {
-        const response = await axios.get("https://localhost:7124/Users");
+        const response = await axios.get("https://localhost:7124/users");
         commit("SET_USERS", response.data);
       } catch (error) {
         console.log(error);
       }
     },
-    async addPet({ commit }, pet) {
+    async loadPets({ commit }) {
+      try {
+        const response = await axios.get("https://localhost:7124/users/1/pets");
+        commit("SET_PETS", response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async addPet({ commit, dispatch }, pet) {
       try {
         await axios.post("https://localhost:7124/users/1/pets", pet);
         commit("ADD_PET", pet);
+        dispatch("loadPets");
       } catch (error) {
         console.log(error);
       }
